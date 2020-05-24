75f73a85834fba8e9f2cb38820aa38214e556e09
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-24 15:44:37 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,7 +6,11 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
     
-    
+    it "should return the current year for a person born in year 0" do
+    age = current_age_for_birth_year(0)
+ 
+  expect(age).to eq(2003)
+end
     
   end
 end
