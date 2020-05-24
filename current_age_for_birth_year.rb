b9dde4b55b73b71de649afc917d12a4e79495c2e
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-24 15:31:29 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,9 @@
 
-def current_age_for_birth_year
+def current_age_for_birth_year(x)
   
   # returns the age of a person based on the year of birth
   
-  
+  age_of_person = current_age_for_birth_year(x)
 
 
 
